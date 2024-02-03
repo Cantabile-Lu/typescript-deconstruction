d1fb4b655c9b8439225c4ea564cc9660edcd9fd0
luwenhai99999 <834299862@qq.com>
2024-02-03 23:07:34 +0800

    feat: 链表添加插入方法

--- packages/Linked/index.ts
+++ packages/Linked/index.ts
@@ -38,6 +38,60 @@ class Linked<T> {
         this.size ++
     }
 
-}
+    /**
+     * @description 插入节点
+     * 1: 插入位置是否合理
+     *   合理: 插入节点
+     *   不合理: 返回false
+     * 2: 插入位置
+     */
+    insert(value: T, position: number){
+        if(position < 0 || position > this.size) return false;
 
+        const node = new Node(value);
+        if(position  === 0 ){
+            // 先将当前节的next指向当前头部节点
+            node.next = this.head;
+            // 更改头部节点
+            this.head = node;
+        }else{
+            let index = 0;
+            // 保存当前头部节点
+            let current = this.head;
+            // 保存上一个节点
+            let pre: Node<T> | null = null;
+            while(index ++ < position){
+                pre = current
+                current = current.next
+            }
+            node.next = current
+            pre.next = node;
+        }
+        this.size ++
+        return true
+    }
+    /**
+     * @description 遍历链表
+     */
+    tranverse(){
+        let current = this.head
+        const values: T[]= []
+        while(current){
+            values.push(current.value)
+            current = current.next
+        }
+        return values
+    }
+
+}
+const lin = new Linked<string>()
+lin.append('aaa');
+lin.append('bbb');
+lin.append('ccc');
+lin.insert("abc", 0)
+const result1 = lin.tranverse()
+console.log(`🚀🚀🚀🚀🚀-> in index.ts on 80`,result1)
+lin.insert("111", 1)
+const result = lin.tranverse()
+console.log(`🚀🚀🚀🚀🚀-> in index.ts on 80`,result)
 export default Node
