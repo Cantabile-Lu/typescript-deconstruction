5a97a0013fe10fcffa10a03f136303586fb5667f
luwenhai99999 <834299862@qq.com>
2024-04-08 07:40:11 +0800

    feat: 循环链表removeAt方法

--- packages/Linked/circularLinked.ts
+++ packages/Linked/circularLinked.ts
@@ -15,6 +15,16 @@ class CircularLinked<T> extends Linked<T> {
         }
          return isSuccess
     }
+
+    override removeAt(position: number): T | null{
+        const value = super.removeAt(position)
+        // 如果删除的是最后一个元素, 并且删除的位置是0
+        if(value && this.tail && (position === 0 || position === this.length)){
+            this.tail.next = this.head
+        }
+
+        return value
+    }
  }
 
  class Person {
@@ -24,7 +34,7 @@ class CircularLinked<T> extends Linked<T> {
 
  const p1 = new Person("张三", 20)
  const p2 = new Person("李四", 18)
- const p3 = new Person("王", 21)
+ const p3 = new Person("王1", 21)
  const p4 = new Person("王五1", 22)
  const p5 = new Person("王2", 23)
 
@@ -36,5 +46,10 @@ class CircularLinked<T> extends Linked<T> {
  circular.traversal()
 // circular.insert(p4, 0)
 circular.insert(p5, 3)
+circular.traversal()
+
+circular.removeAt(0)
+circular.traversal()
+circular.removeAt(2)
 circular.traversal()
  export default CircularLinked
